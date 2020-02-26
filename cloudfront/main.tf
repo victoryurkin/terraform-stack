3dd86309e7479142de4cc4fc27b4fc9b0d641a99
Victor Yurkin <victor.yurkin@aetion.com>
2020-02-26 16:55:23 -0500

    Updates

--- cloudfront/main.tf
+++ cloudfront/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 
 module "cloudfront" {
   source  = "app.terraform.io/victoryurkinpersonal/cloudfront/aws"
-  version = "1.0.20"
+  version = "1.0.21"
 
   client_name = var.client_name
   environment = var.environment
