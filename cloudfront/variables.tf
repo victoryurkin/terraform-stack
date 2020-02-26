2422741dc7cb7cda4d4e71fdcb1a487238d91e4a
Victor Yurkin <victor.yurkin@aetion.com>
2020-02-26 11:04:10 -0500

    Working on cloudfront template

--- cloudfront/variables.tf
+++ cloudfront/variables.tf
@@ -1,3 +1,30 @@
+variable client_name {
+  description = "Name of the client"
+  type        = "string"
+}
+
+variable aws_region {
+  description = "AWS region"
+  type        = "string"
+}
+
+variable environment {
+  description = "Environment name"
+  type        = "string"
+}
+
+variable shared_credentials_file {
+  description = "Shared creds file"
+  type        = "string"
+  default     = "$HOME/.aws/credentials"
+}
+
+variable profile {
+  description = "AWS creds profile"
+  type        = "string"
+  default     = "default"
+}
+
 variable "aliases" {
   type        = "list"
   default     = []
