1257da87375cc4488ec10ed4e7b33c03336f6403
Victor Yurkin <victor.yurkin@aetion.com>
2020-02-27 16:52:11 -0500

    Updates

--- lambda/main.tf
+++ lambda/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 
 module "lambda" {
   source  = "app.terraform.io/victoryurkinpersonal/lambda/aws"
-  version = "1.0.0"
+  version = "1.0.1"
 
   client_name = var.client_name
   environment = var.environment
