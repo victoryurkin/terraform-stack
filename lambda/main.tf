1ab42d7398d602c776f169693609ce46f4a74a82
Victor Yurkin <victor.yurkin@aetion.com>
2020-03-01 11:20:34 -0500

    Updates

--- lambda/main.tf
+++ lambda/main.tf
@@ -14,27 +14,9 @@ provider "aws" {
   profile                 = var.profile
 }
 
-data "archive_file" "lambda_zip_inline" {
-  type        = "zip"
-  output_path = "/tmp/lambda_zip_inline.zip"
-  source {
-    content  = <<EOF
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
-};
-EOF
-    filename = "index.js"
-  }
-}
-
 module "lambda" {
   source  = "app.terraform.io/victoryurkinpersonal/lambda/aws"
-  version = "1.0.7"
+  version = "1.0.8"
 
   client_name = var.client_name
   environment = var.environment
