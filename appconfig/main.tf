f2d35074409900b5e8eadee72319601bbb2ec087
Victor Yurkin <victor.yurkin@aetion.com>
2020-03-03 13:39:58 -0500

    Init

---
+++ appconfig/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  backend "remote" {
+    organization = "victoryurkinpersonal"
+
+    workspaces {
+      name = "terraform-apigateway"
+    }
+  }
+}
+
+provider "aws" {
+  region                  = var.aws_region
+  shared_credentials_file = var.shared_credentials_file
+  profile                 = var.profile
+}
+
+module "appconfig" {
+  source  = "app.terraform.io/victoryurkinpersonal/appconfig/aws"
+  version = "1.0.0"
+
+  client_name = var.client_name
+  environment = var.environment
+  aws_region  = var.aws_region
+}
---
+++ appconfig/var.auto.tfvars
@@ -0,0 +1,3 @@
+client_name  = "baseline"
+aws_region  = "us-east-1"
+environment = "prod"
