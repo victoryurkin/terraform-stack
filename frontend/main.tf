c4a97fc2414d8e1047f06d47a462c1190eca021f
Victor Yurkin <victor.yurkin@aetion.com>
2020-03-04 12:30:29 -0500

    Updates

--- frontend/main.tf
+++ frontend/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 
 module "frontend" {
   source  = "app.terraform.io/victoryurkinpersonal/frontend/aws"
-  version = "1.0.5"
+  version = "1.0.6"
 
   client_name = var.client_name
   environment = var.environment
