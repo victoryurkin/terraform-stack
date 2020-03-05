dc0d722721ad2f04f0ccfe6ea31940acce9d5393
Victor Yurkin <victor.yurkin@aetion.com>
2020-03-05 16:42:52 -0500

    Updtaes

--- appconfig/main.tf
+++ appconfig/main.tf
@@ -30,7 +30,7 @@ module "ssmdocument" {
 
 module "appconfig" {
   source  = "app.terraform.io/victoryurkinpersonal/appconfig/aws"
-  version = "1.0.31"
+  version = "1.0.32"
 
   client_name = var.client_name
   environment = var.environment
