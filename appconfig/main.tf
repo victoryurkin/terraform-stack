554ab81c31ba98ace048b5c34d85609f47e738b2
Victor Yurkin <victor.yurkin@aetion.com>
2020-03-05 14:52:55 -0500

    Updtaes

--- appconfig/main.tf
+++ appconfig/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 
 module "appconfig" {
   source  = "app.terraform.io/victoryurkinpersonal/appconfig/aws"
-  version = "1.0.18"
+  version = "1.0.19"
 
   client_name = var.client_name
   environment = var.environment
