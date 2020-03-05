da980e5d1b8d3b7178891a268bb37da6c49d265a
Victor Yurkin <victor.yurkin@aetion.com>
2020-03-05 16:11:50 -0500

    Updtaes

--- frontend/main.tf
+++ frontend/main.tf
@@ -35,7 +35,7 @@ module "ssmdocument" {
 # AWS AppConfig environment
 module "appconfig" {
   source  = "app.terraform.io/victoryurkinpersonal/appconfig/aws"
-  version = "1.0.26"
+  version = "1.0.27"
 
   client_name = var.client_name
   environment = var.environment
