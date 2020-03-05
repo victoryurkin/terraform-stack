c8bebde317889a0fe5ba925b7704d6919f64ce8b
Victor Yurkin <victor.yurkin@aetion.com>
2020-03-05 12:10:29 -0500

    Updates

--- frontend/main.tf
+++ frontend/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 
 module "ssmdocument" {
   source  = "app.terraform.io/victoryurkinpersonal/ssmdocument/aws"
-  version = "1.0.15"
+  version = "1.0.16"
 
   client_name = var.client_name
   environment = var.environment
