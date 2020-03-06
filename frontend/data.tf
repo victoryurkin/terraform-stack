1e167d930474449064c7603564295acd9b252410
Victor Yurkin <victor.yurkin@aetion.com>
2020-03-06 10:08:19 -0500

    Updates

--- frontend/data.tf
+++ frontend/data.tf
@@ -1,3 +1,3 @@
 data "aws_cloudformation_export" "application_id" {
-  name = "appconfig-application-front-end-config-stack-appconfig-application"
+  name = "front-end-config-stack-appconfig-application"
 }
