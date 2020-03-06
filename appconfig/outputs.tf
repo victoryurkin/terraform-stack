1794fea72744830d7e36fb82a9ecb92bbbf47d3a
Victor Yurkin <victor.yurkin@aetion.com>
2020-03-06 15:50:47 -0500

    Updates

--- appconfig/outputs.tf
+++ appconfig/outputs.tf
@@ -1,4 +1,4 @@
 output "frontend_appconfig_role" {
   description = "Front-end configuration AppConfig application role to access SSM Documents"
-  value       = module.role.this_role_arn
+  value       = module.appconfig.this_role_arn
 }
