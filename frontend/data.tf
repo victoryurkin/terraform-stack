7e1b2a7c11edfe018e592c8ddc261fa7d8d7b56c
Victor Yurkin <victor.yurkin@aetion.com>
2020-03-06 12:06:20 -0500

    Updates

--- frontend/data.tf
+++ frontend/data.tf
@@ -2,6 +2,28 @@ data "aws_cloudformation_export" "application_id" {
   name = "front-end-config-stack-appconfig-application"
 }
 
+data "terraform_remote_state" "dependencies_s3" {
+  backend = "remote"
+
+  config = {
+    organization = "victoryurkinpersonal"
+    workspaces = {
+      name = "terraform-s3"
+    }
+  }
+}
+
+data "terraform_remote_state" "dependencies_apigateway" {
+  backend = "remote"
+
+  config = {
+    organization = "victoryurkinpersonal"
+    workspaces = {
+      name = "terraform-apigateway"
+    }
+  }
+}
+
 data "terraform_remote_state" "dependencies_role" {
   backend = "remote"
 
