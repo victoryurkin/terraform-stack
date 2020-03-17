b3c4df5b8bb002762539fa7a3f652755d86c8d9a
Victor Yurkin <victor.yurkin@aetion.com>
2020-03-17 16:23:20 -0400

    Updates

--- lambda/main.tf
+++ lambda/main.tf
@@ -33,5 +33,20 @@ module "lambda" {
 
   role_name        = var.role_name
   role_policy_name = var.role_policy_name
-  role_statements  = file("${path.module}/policy-statements.json")
+  role_statements  = [
+    {
+      "Action": ["appconfig:GetConfiguration"],
+      "Effect": "Allow",
+      "Resource": "arn:aws:ssm:*"
+    },
+    {
+      "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"        
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:logs:*"
+    }
+  ]
 }
