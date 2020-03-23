eff6507b52b63699067e50fd96bd6d838fe56a49
Victor Yurkin <victor.yurkin@aetion.com>
2020-03-23 14:39:32 -0400

    Updates

--- frontend/main.tf
+++ frontend/main.tf
@@ -56,7 +56,7 @@ module "appconfig" {
 
 module "cloudfront" {
   source  = "app.terraform.io/victoryurkinpersonal/cloudfront/aws"
-  version = "1.0.41"
+  version = "1.0.42"
 
   client_name         = var.client_name
   aws_region          = var.aws_region
