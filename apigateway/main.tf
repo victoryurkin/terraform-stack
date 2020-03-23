89a3ce77a8bb5da06a2fd0626b3677f35aa5af39
Victor Yurkin <victor.yurkin@aetion.com>
2020-03-23 10:13:28 -0400

    Updates

--- apigateway/main.tf
+++ apigateway/main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 
 module "apigateway" {
   source  = "app.terraform.io/victoryurkinpersonal/apigateway/aws"
-  version = "1.0.36"
+  version = "1.0.37"
 
   client_name         = var.organization_name
   aws_region          = var.aws_region
