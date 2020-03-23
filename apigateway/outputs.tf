2d181fdb50d58e164e6f74b738185c956406922d
Victor Yurkin <victor.yurkin@aetion.com>
2020-03-23 09:15:59 -0400

    Updates

--- apigateway/outputs.tf
+++ apigateway/outputs.tf
@@ -2,3 +2,8 @@ output "frontend_config_api_gateway_invoke_url" {
   description = "Front-end configuration service API Gateway"
   value       = replace(replace(module.apigateway.frontend_config_api_gateway_deployment_invoke_url, "https://", ""), "/config", "")
 }
+
+output "frontend_config_api_gateway_lambda_permission_source_arn" {
+  description = "Source ARN"
+  value       = module.apigateway.frontend_config_api_gateway_lambda_permission_source_arn
+}
